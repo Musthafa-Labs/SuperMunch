e1aac97402d763da219cca679a95ecea032c00c1
Musthafa-Labs <mohammedmusthafa942006@gmail.com>
2026-03-01 15:18:56 +0530

    Remove listen for serverless

--- api/index.ts
+++ api/index.ts
@@ -91,13 +91,6 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-  {
-    port,
-    host: "0.0.0.0",
-  },
-  () => {
-    log(`serving on port ${port}`);
-  }
-);
+ // your routes above
 })();
+export default app;
